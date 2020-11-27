dc2fb3d23cff8fff32bc9f9e8f01b655b9f81752
rotznmu <rotznmu@gmail.com>
2020-11-27 13:08:52 -0500

    added instructions

--- script.test.js
+++ script.test.js
@@ -1,3 +1,7 @@
+/* it('write what we expect a given function to do', function() {
+    expect(some_function(argument)).toEqual(expected_outcome)
+}) */
+
 it("should shuffle the array", function() {
     expect(1+1).toEqual(2);
 })
